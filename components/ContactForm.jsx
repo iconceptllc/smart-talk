2c0af2a18239e4ec75a20ff5e367b1ae835b0a3e
Sanilal P Sathyan <sanilal@gmail.com>
2024-07-18 17:27:17 +0400

    captcha added

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -1,6 +1,7 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import ReCAPTCHA from 'react-google-recaptcha';
 
 const ContactForm = () => {
   const [name, setName] = useState('');
@@ -10,9 +11,20 @@ const ContactForm = () => {
   const [services, setServices] = useState('');
   const [message, setMessage] = useState('');
   const [formStatus, setFormStatus] = useState('');
+  const [captchaToken, setCaptchaToken] = useState('');
+  const recaptchaRef = useRef(null);
+
+  const handleCaptchaChange = (token) => {
+    setCaptchaToken(token);
+  };
 
   const sendEmail = (e) => {
     e.preventDefault();
+
+    if (!captchaToken) {
+      setFormStatus('error');
+      return;
+    }
 
     const templateParams = {
       from_name: name,
@@ -22,6 +34,7 @@ const ContactForm = () => {
       from_service: services,
       to_name: 'Smart Talk',
       message: message,
+      'g-recaptcha-response': captchaToken,
     };
 
     emailjs.send(
@@ -39,6 +52,7 @@ const ContactForm = () => {
       setServices('');
       setMessage('');
       setFormStatus('success');
+      recaptchaRef.current.reset();
     })
     .catch((error) => {
       console.error('Error sending email:', error);
@@ -108,6 +122,14 @@ const ContactForm = () => {
               placeholder="Your Message"
               onChange={(e) => setMessage(e.target.value)}
             ></textarea>
+          </div>
+          <div className="flex flex-col sm:col-span-2">
+            <ReCAPTCHA
+              ref={recaptchaRef}
+              sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+              size="invisible"
+              onChange={handleCaptchaChange}
+            />
           </div>
           <div className="flex justify-left sm:col-span-2">
             <button
