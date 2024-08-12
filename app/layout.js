390957bbdd5a898f6751dc2ffaf7a8dcd782ab83
Sanilal P Sathyan <sanilal@gmail.com>
2024-08-12 15:08:47 +0400

    gtm event

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Hero from "@/components/Hero";
 import { GoogleTagManager } from '@next/third-parties/google' 
+import { sendGTMEvent } from '@next/third-parties/google' 
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
        <GoogleTagManager gtmId="AW-1047332511" />
+       
       <body className={inter.className}>
       <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=AW-1047332511"
 height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
@@ -22,6 +24,18 @@ height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe>
           <Hero />
           {children}
         </div>
+         {/* Insert custom script for sending GTM event */}
+         <script dangerouslySetInnerHTML={{
+          __html: `
+            window.addEventListener('load', function() {
+              window.dataLayer = window.dataLayer || [];
+              window.dataLayer.push({
+                event: 'conversion',
+                send_to: 'AW-1047332511/chHACJXczLgZEJ-NtPMD'
+              });
+            });
+          `,
+        }} />
         </body>
     </html>
   );
